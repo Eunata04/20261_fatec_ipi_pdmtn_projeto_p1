79221334ac9aedea9885358b04feded1f5fc5399
Eunata04 <eunata.117@gmail.com>
2026-04-16 23:51:49 -0300

    feat(p1): estrutura inicial com app e bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <h1>Hello, Investimentos</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
